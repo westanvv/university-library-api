5b66e828660720bd89a0a4f1b4c2817dc13eed98
Vadym Nemchenko <westa.nvv@gmail.com>
2023-06-13 16:19:04 +0300

    Cleanup

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -7,9 +7,9 @@ import PERMISSIONS from 'constants/permissions';
 
 const router = new Router();
 
-router.get('/', checkAuth([PERMISSIONS.POSTS_READ]), helpers.wrapRoute(PostController.getAll));
+router.get('/', helpers.wrapRoute(PostController.getAll));
 router.post('/', checkAuth([PERMISSIONS.POSTS_CREATE]), helpers.wrapRoute(PostController.create));
-router.get('/:postId', checkAuth([PERMISSIONS.POSTS_READ]), helpers.wrapRoute(PostController.getItem));
+router.get('/:postId', helpers.wrapRoute(PostController.getItem));
 router.put('/:postId', checkAuth([PERMISSIONS.POSTS_UPDATE]), helpers.wrapRoute(PostController.update));
 router.delete('/:postId', checkAuth([PERMISSIONS.POSTS_DELETE]), helpers.wrapRoute(PostController.delete));
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -11,7 +11,7 @@ router.get('/', checkAuth([PERMISSIONS.USERS_READ]), helpers.wrapRoute(UserContr
 router.post('/', checkAuth([PERMISSIONS.USERS_CREATE]), helpers.wrapRoute(UserController.create));
 router.get('/:userId', checkAuth([PERMISSIONS.USERS_READ]), helpers.wrapRoute(UserController.getItem));
 router.put('/:userId', checkAuth([PERMISSIONS.USERS_UPDATE]), helpers.wrapRoute(UserController.update));
-router.put('/:userId/role', checkAuth(), helpers.wrapRoute(UserController.updateRole));
+router.put('/:userId/role', helpers.wrapRoute(UserController.updateRole));
 router.delete('/:userId', checkAuth([PERMISSIONS.USERS_DELETE]), helpers.wrapRoute(UserController.delete));
 
 export default router;
